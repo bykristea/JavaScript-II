c53e434f80b1fd61fad30ec94577643b66d41288
Kristea Gamel <krgamel@gmail.com>
2019-01-19 12:58:07 -0600

    completed array-mathods

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,21 +56,43 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+runners.forEach(function (object){
+    let name = object.first_name + " " + object.last_name;
+    fullName.push(name)
+})
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
+runners.map(function (object,index,arr) {
+allCaps.push(arr[index].first_name.toUpperCase())   
+})
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+
+const sizeFltr = function(object, index, arr) {
+    if (arr[index].shirt_size === "L") {
+        return true;
+    }
+    return false;
+}
+
+let largeShirts = runners.filter(sizeFltr);
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+const donations = runners.map(function(object, index, arr) {
+    return arr[index].donation;
+})
+
+let ticketPriceTotal = donations.reduce(function(acc, value){
+    return acc + value;
+});
+
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
